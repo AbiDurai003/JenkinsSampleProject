9b13da780db822df4dee51bcac25788be335f115
AbiDurai003 <Aara Suthar@AaraSuthar>
2025-03-03 11:11:29 +0530

    Initial Commit

---
+++ JenkinsFirstProject/src/main/java/SampleProject/JenkinsSampleProject.java
@@ -0,0 +1,13 @@
+package SampleProject;
+
+public class JenkinsSampleProject {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Test");
+		System.out.println("Testing");
+		System.out.println("Tester");
+
+	}
+
+}
